48f646dcdab78384dab892bc84ff88f8f0c3f8ea
BigMac <teamautomatons2823@gmail.com>
2023-01-16 18:00:10 -0600

    added odometry, autonomous, made command based, cleanup, massive changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,55 +7,53 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends TimedRobot {
-  //private final XboxController m_controller = new XboxController(0);
-  private final Joystick m_joystick = new Joystick(1);
-  private final DriveSubsystem m_swerve = new DriveSubsystem();
+  // private final XboxController m_controller = new XboxController(0);
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
   private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(1);
   private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(1);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(1);
 
+  private RobotContainer m_robotContainer;
+  private Command m_autonomousCommand;
+
   @Override
-  public void testPeriodic() {
-    //m_swerve.drive(0, 0.2, 0, false);
-    driveWithJoystick(false);
-    //System.out.println("Im in test mode");
+  public void robotInit() {
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
   @Override
-  public void teleopPeriodic() {
-    driveWithJoystick(true);
+  public void robotPeriodic() {
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
-  private void driveWithJoystick(boolean fieldRelative) {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    final var xSpeed =
-        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_joystick.getRawAxis(1), 0.02))
-            * DriveSubsystem.kMaxSpeed;
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
-    final var ySpeed =
-        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_joystick.getRawAxis(0), 0.02))
-            * DriveSubsystem.kMaxSpeed;
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    final var rot =
-        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_joystick.getRawAxis(2), 0.02))
-            * DriveSubsystem.kMaxAngularSpeed;
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
 
-    m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,81 +4,135 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.ctre.phoenix.sensors.PigeonIMU.CalibrationMode;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.wpilibj.AnalogGyro;
-import edu.wpi.first.wpilibj.SerialPort.Port;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.SwerveModule;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import com.fasterxml.jackson.databind.JsonSerializable.Base;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.*;
+import frc.robot.SwerveModule;
 
 /** Represents a swerve drive style drivetrain. */
-public class DriveSubsystem {
+public class DriveSubsystem extends SubsystemBase {
   public static final double kMaxSpeed = 4.0; // 3 meters per second
-  public static final double kMaxAngularSpeed = Math.PI*4; // 1/2 rotation per second
+  public static final double kMaxAngularSpeed = Math.PI * 4; // 1/2 rotation per second
 
-  private final Translation2d m_frontLeftLocation = new Translation2d(0.244, 0.244);
-  private final Translation2d m_frontRightLocation = new Translation2d(0.244, -0.244);
-  private final Translation2d m_backLeftLocation = new Translation2d(-0.244, 0.244);
-  private final Translation2d m_backRightLocation = new Translation2d(-0.244, -0.244);
+  private final SwerveModule m_frontLeft = new SwerveModule(13, 12); // BIG BONGO 2
+  private final SwerveModule m_frontRight = new SwerveModule(2, 3); // BIG BONGO 1
+  private final SwerveModule m_backLeft = new SwerveModule(14, 15); // BIG BONGO 3
+  private final SwerveModule m_backRight = new SwerveModule(50, 1); // BIG BONGO 4
 
-  private final SwerveModule m_frontLeft = new SwerveModule(13, 12); //BIG BONGO 2
-  private final SwerveModule m_frontRight = new SwerveModule(2, 3); //BIG BONGO 1
-  private final SwerveModule m_backLeft = new SwerveModule(14, 15); //BIG BONGO 3
-  private final SwerveModule m_backRight = new SwerveModule(50, 1); //BIG BONGO 4
-
-  ///private final DutyCycleEncoder m_testEncoder = new DutyCycleEncoder(id);
+  /// private final DutyCycleEncoder m_testEncoder = new DutyCycleEncoder(id);
   private final DutyCycleEncoder m_frontLeftEncoder = new DutyCycleEncoder(11);
   private final DutyCycleEncoder m_frontRighttEncoder = new DutyCycleEncoder(12);
   private final DutyCycleEncoder m_backLeftEncoder = new DutyCycleEncoder(13);
   private final DutyCycleEncoder m_backRightEncoder = new DutyCycleEncoder(14);
-  //Duty Encoders may have the wrong values
+
+  private final Field2d m_field = new Field2d();
+  // Duty Encoders may have the wrong values
 
   private final PigeonIMU m_pGyro = new PigeonIMU(57);
 
-  private final SwerveDriveKinematics m_kinematics =
-      new SwerveDriveKinematics(
-          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+  SwerveDriveOdometry m_odometry;
 
   public DriveSubsystem() {
     m_pGyro.setYaw(0);
+    var pigeonYaw = new Rotation2d(Math.toRadians(m_pGyro.getYaw()));
+    m_odometry = new SwerveDriveOdometry(
+        DriverConstants.kDriveKinematics,
+        pigeonYaw,
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_backLeft.getPosition(),
+            m_backRight.getPosition()
+        });
   }
-  
+
+  @Override
+  public void periodic() {
+    var pigeonYaw = new Rotation2d(Math.toRadians(m_pGyro.getYaw()));
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        pigeonYaw,
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_backLeft.getPosition(),
+            m_backRight.getPosition()
+        });
+    m_field.setRobotPose(getPose());
+    System.out.println(getPose().getX());
+  }
+
   /**
-   * Method  to  drive  the  robot  using  joystick  info.
+   * Method to drive the robot using joystick info.
    *
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the ro bot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the ro bot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
    */
   @SuppressWarnings("ParameterName")
   public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
     var pigeonYaw = new Rotation2d(Math.toRadians(m_pGyro.getYaw()));
-    var swerveModuleStates =
-        m_kinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, pigeonYaw)
-                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    var swerveModuleStates = DriverConstants.kDriveKinematics.toSwerveModuleStates(
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, pigeonYaw)
+            : new ChassisSpeeds(xSpeed, ySpeed, rot));
     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    setModuleStates(swerveModuleStates);
+  }
+
+  public void resetEncoders() {
+    m_frontLeft.resetEncoderPosition(RobotConstants.swerveOffset);
+    m_frontRight.resetEncoderPosition(RobotConstants.swerveOffset);
+    m_backLeft.resetEncoderPosition(RobotConstants.swerveOffset);
+    m_backRight.resetEncoderPosition(RobotConstants.swerveOffset);
+
+    try {
+      Thread.sleep(1);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    m_frontLeft.resetEncoderValue();
+    m_frontRight.resetEncoderValue();
+    m_backLeft.resetEncoderValue();
+    m_backRight.resetEncoderValue();
+  }
+
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  public void setModuleStates(SwerveModuleState[] swerveModuleStates) {
     m_frontLeft.setDesiredState(swerveModuleStates[0]);
     m_frontRight.setDesiredState(swerveModuleStates[1]);
     m_backLeft.setDesiredState(swerveModuleStates[2]);
     m_backRight.setDesiredState(swerveModuleStates[3]);
-    //System.out.println("X Speed: " + xSpeed + " | Y Speed: " + ySpeed + " | Rotation: " + rot);
-    //System.out.println(m_gyro.getYaw());
-    //System.out.println("Swerve Stuff" + swerveModuleStates[0].angle + " y " + ySpeed);
-    
-    //SmartDashboard.putNumber("Duty Encoder", m_testEncoder.get());
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    var pigeonYaw = new Rotation2d(Math.toRadians(m_pGyro.getYaw()));
+    m_odometry.resetPosition(
+        pigeonYaw,
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_backLeft.getPosition(),
+            m_backRight.getPosition()
+        },
+        pose);
   }
 }
-
-
