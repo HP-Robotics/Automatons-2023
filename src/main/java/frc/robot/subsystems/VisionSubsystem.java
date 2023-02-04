1a1bda016f5634ec877620b5214ecd4f2218c839
Otto <teamautomatons2823@gmail.com>
2023-02-04 13:43:58 -0600

    camera position and rotation working

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -35,13 +35,11 @@ public class VisionSubsystem extends SubsystemBase {
   GenericEntry robotPose;
   NetworkTableEntry yEntry;
   NetworkTableEntry targetPose;
-  Transform2d apriltagPose;
+  Pose2d apriltagPose;
   Pose2d absolutePose;
-  Pose2d cameraPose;
-  Map<Integer, Transform2d> apriltags;
+  Transform2d cameraTrans;
+  Map<Integer, Pose2d> apriltags;
   int apriltageid;
-  Translation2d cameraTrans;
-  Pose2d rotationCamPose;
 
   private Field2d m_field = new Field2d();
   PhotonCamera camera = new PhotonCamera(Constants.VisionConstants.kcameraName);
@@ -53,15 +51,15 @@ public class VisionSubsystem extends SubsystemBase {
   public VisionSubsystem() {
 
     SmartDashboard.putData("Field", m_field);
-    apriltags = new HashMap<Integer, Transform2d>();
-    apriltags.put(1, new Transform2d(new Translation2d(15.514, 1.072), new Rotation2d(Math.PI)));
-    apriltags.put(2, new Transform2d(new Translation2d(15.514, 2.748), new Rotation2d(Math.PI)));
-    apriltags.put(3, new Transform2d(new Translation2d(15.514, 4.424), new Rotation2d(Math.PI)));
-    apriltags.put(4, new Transform2d(new Translation2d(16.179, 6.75), new Rotation2d(Math.PI)));
-    apriltags.put(5, new Transform2d(new Translation2d(0.362, 6.75), new Rotation2d(0)));
-    apriltags.put(6, new Transform2d(new Translation2d(1.027, 4.424), new Rotation2d(0)));
-    apriltags.put(7, new Transform2d(new Translation2d(1.027, 2.748), new Rotation2d(0)));
-    apriltags.put(8, new Transform2d(new Translation2d(1.027, 1.072), new Rotation2d(0)));
+    apriltags = new HashMap<Integer, Pose2d>();
+    apriltags.put(1, new Pose2d(new Translation2d(15.514, 1.072), new Rotation2d(Math.PI)));
+    apriltags.put(2, new Pose2d(new Translation2d(15.514, 2.748), new Rotation2d(Math.PI)));
+    apriltags.put(3, new Pose2d(new Translation2d(15.514, 4.424), new Rotation2d(Math.PI)));
+    apriltags.put(4, new Pose2d(new Translation2d(16.179, 6.75), new Rotation2d(Math.PI)));
+    apriltags.put(5, new Pose2d(new Translation2d(0.362, 6.75), new Rotation2d(0)));
+    apriltags.put(6, new Pose2d(new Translation2d(1.027, 4.424), new Rotation2d(0)));
+    apriltags.put(7, new Pose2d(new Translation2d(1.027, 2.748), new Rotation2d(0)));
+    apriltags.put(8, new Pose2d(new Translation2d(1.027, 1.072), new Rotation2d(0)));
 
     // robotPose = Shuffleboard.getTab("Field")
     // .add("CameraPose", new Pose2d())
@@ -101,12 +99,11 @@ public class VisionSubsystem extends SubsystemBase {
         if (apriltagPose == null) {
           return null;
         }
-        cameraPose = new Pose2d(target.getBestCameraToTarget().getX(), target.getBestCameraToTarget().getY(),
-            new Rotation2d());
-        cameraTrans = cameraPose.getTranslation().rotateBy(apriltagPose.getRotation());
-        rotationCamPose = new Pose2d(cameraTrans, new Rotation2d());
+        cameraTrans = new Transform2d(
+            new Translation2d(target.getBestCameraToTarget().getX(), target.getBestCameraToTarget().getY()),
+            (target.getBestCameraToTarget().getRotation().toRotation2d()));
         // robotPose.setValue(absolutePose);
-        absolutePose = rotationCamPose.plus(apriltagPose);
+        absolutePose = apriltagPose.plus(cameraTrans);
         m_field.setRobotPose(absolutePose);
 
         return absolutePose;
