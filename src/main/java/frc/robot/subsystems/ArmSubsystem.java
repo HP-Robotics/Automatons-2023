a0b768b577c8a13449150eaa8529b92b9c9bd1bd
BigMac <teamautomatons2823@gmail.com>
2023-03-02 15:20:07 -0600

    Arm moving up and down from high skip middle state

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -160,7 +160,11 @@ public class ArmSubsystem extends SubsystemBase {
 
   public void moveDownState() {
     if (m_currentState == m_pastState && m_currentState != ArmConstants.intakeState) {
-      m_currentState--;
+      if (m_targetState == ArmConstants.stowState && m_currentState == ArmConstants.highState) {
+        m_currentState = ArmConstants.lowState;
+      } else {
+        m_currentState--;
+      }
     }
 
     m_isChanging = true;
@@ -175,7 +179,11 @@ public class ArmSubsystem extends SubsystemBase {
 
   public void moveUpState() {
     if (m_currentState == m_pastState && m_currentState != ArmConstants.scoreState) {
-      m_currentState++;
+      if (m_targetState == ArmConstants.highState && m_pastState == ArmConstants.lowState) {
+        m_currentState = ArmConstants.highState;
+      } else {
+        m_currentState++;
+      }
     }
 
     m_isChanging = true;
